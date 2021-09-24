94c357ebb5a49b0c388ac5be12cbfc15263098fe
Grigor Grigoryan <>
2021-09-24 22:20:40 +0200

    Setup folder structure

---
+++ Weather/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Grigor Grigoryan on 24.09.21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
